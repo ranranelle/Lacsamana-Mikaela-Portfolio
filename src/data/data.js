caf3f0b3b4bfe0e00d72ccb6108e425f04de8fdb
Mikaela Lacsamana <mika.lacsamana@ucalgary.ca>
2025-01-14 00:25:57 -0700

    Updated Portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,11 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
+    name: "Mikaela Lacsamana",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
+        email: "mikaela.lacsamana@ucalgary.ca",
+        phone: "+ 1 (587)-284-0609",
         linkedin: "https://www.linkedin.com/",
         github: "https://www.github.com/",
     }
